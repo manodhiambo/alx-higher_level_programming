b8e40db67bfa2951fa81d7987868cc85189c7ff5
manodhiambo <manodhiambo@gmail.com>
2021-11-24 14:37:42 +0300

    fix 0-square_matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-        temp_list = list(map(lambda x: x * x, i))
-        new_matrix.append(temp_list)
-        return (new_matrix)
+        tmp_list = list(map(lambda x: x * x, i))
+        new_matrix.append(tmp_list)
+    return (new_matrix)
