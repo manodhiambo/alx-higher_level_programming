79048dfc274ae3d8fa0773246186a5b22b1aaf60
manodhiambo <manodhiambo@gmail.com>
2021-11-24 14:39:56 +0300

    fix 1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,4 @@ def search_replace(my_list, search, replace):
     for idx in range(len(my_list)):
         if (my_list[idx] == search):
             new_list[idx] = replace
-            return (new_list)
+    return (new_list)
