04369f2a5794c8cbc2648ed08a4fa8d5b8faf504
manodhiambo <manodhiambo@gmail.com>
2021-12-14 19:51:14 -0800

    committing fixed codes

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 """
-Program that Ident a text according to '.' and '?' symbol
+Function that prints a text with 2 new lines after each of these characters:
+. ? and :
 """
 
 
 def text_indentation(text):
-    """ A function that prints a text with 2 new lines after
-    each of these characters: ., ?
     """
-    if (type(text) is not str or text is None):
-        raise TypeError("text must be a string")
-    flag = 0
-    for c in text:
-        if (c == '.' or c == '?' or c == ':'):
-            print(c, end='')
-            print('')
-            print('')
-            flag = 1
-        else:
-            if (flag == 0):
-                print(c, end='')
-            else:
-                if (c == ' ' or c == '\t'):
-                    pass
-                else:
-                    print(c, end="")
-                    flag = 0
+    Prints a text replace some characters with 2 newlines
+    Arguments:
+    text: must be a string
+    If not a string a TypeError is raised
+    There is no space at the beginning at the end of each printed line
+    """
+
+    if not isinstance(text, str):
+        msg = "text must be a string"
+        raise TypeError(msg)
+
+    if len(text) == 0:
+        print(end='')
+        return None
+
+    textlist = []
+    mark = 0
+
+    for i in range(len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            textlist.append(text[mark: i + 1] + "\n\n")
+            mark = i + 1
+
+    textlist.append(text[mark:i + 1])
+
+    textlist = [i.strip(' ') for i in textlist]
+
+    new_text = "".join(textlist)
+
+    print(new_text, end='')
