8294894d8b1ed030d903af52a79ca98691ac2105
manodhiambo <manodhiambo@gmail.com>
2021-12-14 19:39:16 -0800

    committing fixed codes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 """
-A function that divides all elements of a matrix
-All elements of the matrix should be divided by div, rounded to 2 decimal places
-Returns a new matrix
+Return a new matrix Divided by div value
 """
 
 
 def matrix_divided(matrix, div):
-     """
-    A function that divides all elements of a matrix.
     """
+    function that divides all elements of a matrix.
+    """
+    s = "matrix must be a matrix (list of lists) of integers/floats"
     if not (matrix or isinstance(matrix, list)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     if not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError("div must be a number")
     if (div == 0):
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         if (len(row) == 0):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(s)
         if (len(row) != len(matrix[0])):
             raise TypeError("Each row of the matrix must have the same size")
         if not (row or isinstance(row, list)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(s)
         for col in row:
             if not (isinstance(col, int) or isinstance(col, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(s)
     new_matrix = [[round((col / div), 2) for col in row] for row in matrix]
     return (new_matrix)
