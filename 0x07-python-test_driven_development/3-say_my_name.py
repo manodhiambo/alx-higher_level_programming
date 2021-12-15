1c4f11c951674625c42d82491534ba671d7564cd
manodhiambo <manodhiambo@gmail.com>
2021-12-14 20:22:05 -0800

    committing fixed codes

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """
-Program that prints the name with edge cases
+Function that prints My name is <first name> <last name>
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """ Function that prints our name """
-    if not (isinstance(first_name, str)):
-        raise TypeError("first_name must be a string")
-    if not (isinstance(last_name, str)):
-        raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    """
+    Prints a Name
+    Arguments:
+    first_name: must be a string
+    last_name: must be a string, if no argument given, then empty by default
+    Otherwise a TypeError is raised
+    """
+    if not isinstance(first_name, str):
+        msg = "first_name must be a string"
+        raise TypeError(msg)
+
+    if not isinstance(last_name, str):
+        msg = "last_name must be a string"
+        raise TypeError(msg)
+
+    print("My name is {} {}".format(first_name, last_name))
