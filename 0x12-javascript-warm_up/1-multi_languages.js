ff010082bc686b36178ba02f2207787da6db1b72
manodhiambo <manodhiambo@gmail.com>
2022-02-28 09:34:35 -0800

    Fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // script that prints 3 lines
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
