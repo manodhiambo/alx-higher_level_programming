56fe5975076dc3b69939300390cff3f39e672198
manodhiambo <manodhiambo@gmail.com>
2022-03-01 01:47:50 -0800

    Fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-/* a script that imports a dictionary of occurrences by user id 
- * and computes a dictionary of user ids by occurrence */
+// a script that imports a dictionary of occurrences by user id 
 const dict = require('./101-data').dict;
 const newDict = {};
 
