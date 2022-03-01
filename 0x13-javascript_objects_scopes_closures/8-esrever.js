b3f210c4ba1e17763ec7dda27abba59ce347f52b
manodhiambo <manodhiambo@gmail.com>
2022-03-01 01:09:41 -0800

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+  const result = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    result.push(list[i]);
+  }
+  return result;
+};
