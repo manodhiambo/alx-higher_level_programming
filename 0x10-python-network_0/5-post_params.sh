4f98d15a09cb83f7bbf2fd9ce37e1ebc62a10a13
manodhiambo <manodhiambo@gmail.com>
2022-03-17 12:25:43 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
-cript that takes in a URL, sends a POST request to the passed URL
+#!/bin/bash
+#A Script that takes in a URL, sends a POST request to the passed URL
 curl -LsX POST "$1" -d "email=hr@test@gmail.com&subject=I+will+always+be+here+for+PLD"
