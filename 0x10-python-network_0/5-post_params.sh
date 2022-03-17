b91b865446182c934f4ed3c4999f0cb0d9f913c6
manodhiambo <manodhiambo@gmail.com>
2022-03-17 12:29:03 +0000

    Fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Script that takes in a URL, sends a POST request to the passed URL
-curl -LsX POST "$1" -d "email=hr@test@gmail.com&subject=I+will+always+be+here+for+PLD"
+curl -s "$1" -X POST -d 'email=hr@test@gmail.com&subject=I will always be here for PLD'
