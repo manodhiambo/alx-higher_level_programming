1e97b64f23eae50b618a1cf259af8fd41d7692fb
manodhiambo <manodhiambo@gmail.com>
2022-03-17 12:42:52 +0000

    Fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -sL -X PUT -d user_id=98 -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin:You got me!"
