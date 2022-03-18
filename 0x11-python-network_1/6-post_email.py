17b8cd01e5a3445b05bf251aaae1806d9dd8ce75
manodhiambo <manodhiambo@gmail.com>
2022-03-18 17:16:37 +0000

    Fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script that sends a POST request to the passed URL with the email as a parameter """
+""" Sends a POST request to the passed URL with the email as a parameter """
 import requests
 from sys import argv
 
