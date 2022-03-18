9535a8bd8ca3b9cfd93baaf199c86c5645a21c6f
manodhiambo <manodhiambo@gmail.com>
2022-03-18 17:06:46 +0000

    GitHub evaluation fot backend test

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+""" A script that sends a POST request to Github repo API """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    rep_name = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/' + owner + '/' + rep_name + '/commits'
+    req = requests.get(url)
+    user_json = req.json()
+    counter = 0
+    for d in user_json:
+        if (counter < 10):
+            if (type(d) is dict):
+                print("{}: {}".format(d['sha'],
+                                      d['commit'].get('author').get('name')))
+            counter += 1
+        else:
+            break
