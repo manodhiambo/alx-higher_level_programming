cdad9228da1e7c49bb18c862f7c0f982ebec4b4d
manodhiambo <manodhiambo@gmail.com>
2022-03-18 16:41:02 +0000

    A script that displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A script that sends a POST request to the passed URL with the email as a parameter """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    payload = {'email': email}
+    req = requests.post(url, data=payload)
+    print(req.text)
