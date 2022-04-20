a6467577e01c0690b633259dd76964cba746ff50
manodhiambo <manodhiambo@gmail.com>
2022-04-20 17:30:48 +0000

    4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('div#toggle_header').click(function () {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green');
+      $('header').addClass('red');
+    } else {
+      $('header').removeClass('red');
+      $('header').addClass('green');
+    }
+  });
+});
